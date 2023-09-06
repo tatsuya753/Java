5fee450cafddda700f0e5440ec5bc0b1a81801ec
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-09-06 17:52:20 +0000

    変数宣言と参照

---
+++ Java1/Variable.java
@@ -0,0 +1,15 @@
+//クラス
+class Variable{
+  //メソッド
+  public static void main(String[] args){
+    // 変数の宣言
+    int month;
+    // 変数の代入
+    month = 9;
+    // 変数の初期化
+    int day = 16;
+
+    System.out.println("私の誕生日は");  //画面の表示
+    System.out.println(month + "月" + day + "日です！！");  //変数の参照
+  }
+}
