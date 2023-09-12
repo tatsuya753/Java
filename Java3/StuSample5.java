4fd17f2612822d67e1745b82eb1e404c71c27cd2
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-09-12 01:44:44 +0000

    スーパーとサブの型変換

---
+++ Java3/StuSample5.java
@@ -0,0 +1,17 @@
+public class StuSample5{
+  public static void main(String[] args){
+    Student5 stu1 = new Student5("宗像", 1);
+    Person5 psn = stu1;
+    psn.display
+    //psn.chgStuNo(1001);　コンパイルエラーになる 
+
+    /*補足：instanceofキーワード
+    オブジェクトのクラスを特定する
+    対象のオブジェクト　instanceof　クラス名　*/
+    if(psn instanceof Student5){
+      Student5 stu2 = (Student5)psn;
+      stu2.chgStuNo(1001);
+      stu2.display();
+    }
+  }
+}
