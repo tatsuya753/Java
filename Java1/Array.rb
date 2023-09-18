827d2b5cd8c1aafc6eabf8d3468a3f1d19ffa442
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-09-18 03:18:05 +0000

    ruby

---
+++ Java1/Array.rb
@@ -0,0 +1,8 @@
+score = [80, 100, 75]
+name = ["鈴木", "田中", "宗像"]
+
+puts name[0] + "さん" + "#{score[0]}" + "点"
+puts name[1] + "さん" + "#{score[1]}" + "点"
+puts name[2] + "さん" + "#{score[2]}" + "点"
+puts "受験者数" + score.length.to_s + "名"
+puts "得点合計：" + "#{score[0] + score[1] + score[2]}"
