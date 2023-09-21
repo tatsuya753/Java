cf6c6d1f10a5342ebdd9772f2ae5512a4f35b0bd
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-09-21 12:24:09 +0000

    Add message

--- Java1/Array.rb
+++ Java1/Array.rb
@@ -6,5 +6,7 @@ name = ["鈴木", "田中", "宗像"]
 puts name[0] + "さん" + "#{score[0]}" + "点"
 puts name[1] + "さん" + "#{score[1]}" + "点"
 puts name[2] + "さん" + "#{score[2]}" + "点"
+
+# 点数合計表示
 puts "受験者数" + score.length.to_s + "名"
 puts "得点合計：" + "#{score[0] + score[1] + score[2]}"
