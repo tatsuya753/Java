9c71a8d9f871384621a34262c34a6c321cc37b3d
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-10-07 18:51:39 +0000

    Fix

--- ruby.rb
+++ ruby.rb
@@ -1,8 +1,11 @@
 # 入力させるgetsメッソド
 line = gets.to_i  #to_iでint型に変換
+
 # line(指定回数分繰り返すtimesメッソド)
 line.times do |line_count| #繰り返しの回数を知る為
+
 # 出力、繰り返し回数の表示
   puts "Hello : " + "#{line_count + 1}"  #繰り返し回数を1から表示
+
 end
 
