d1f04398c32945363b518243244769cfcfe2ba35
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-10-09 03:42:20 +0000

    equals

---
+++ JavaSample/Main1_4.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main1_4{
+  public static void main(String[] args){
+    int[] a = {1, 2, 3, 4, 5};
+    int[] b = {1, 2, 3, 4, 5};
+
+    System.out.println("誤った判定" + a.equals(b));
+    System.out.println("正しい判定" + Arrays.equals(a, b));
+  }
+}
