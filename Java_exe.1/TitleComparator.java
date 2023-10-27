5049853e7cd8f7062650d56716153e7e7f0e1074
EC2 Default User <133572109+tatsuya753@users.noreply.github.com>
2023-10-27 16:38:52 +0000

    Fix comment

--- Java_exe.1/TitleComparator.java
+++ Java_exe.1/TitleComparator.java
@@ -1,7 +1,6 @@
 // 問題1-2
-//書名の昇順での並び替え
 import java.util.Comparator;
-
+//書名の昇順での並び替え
 public class TitleComparator implements Comparator<Book> {
   public int compare(Book x, Book y) {
     return x.getTitle().compareTo(y.getTitle());
